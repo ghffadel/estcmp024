37a5fee55ddf3e3f3d2c9cdce3d983224000b816
ghffadel <ghff.snf20@uea.edu.br>
2022-02-07 22:31:01 -0400

    Fixed some bugs

--- FullStackApp/backend/index.js
+++ FullStackApp/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
 app.use(express.json())
 app.use(cors())
@@ -17,8 +18,13 @@ app.use('/likes', likesRouter)
 app.use('/posts', postsRouter)
 app.use('/auth', usersRouter)
 
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log('Server running on port 3001')
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+      console.log('Server running on port 3001')
+  })
+  .catch((err) => {
+    console.log(err)
   })
 })
